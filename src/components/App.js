45fba3762a88f25cc4a549a4bbacba2d9a062581
ceigh@pm.me <Artjom Lbsack>
2019-09-11 12:20:03 +0500

    feat: Markup

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Input from './Input';
+import Modes from './Modes';
+import Canvas from './Canvas';
+
+export default function App() {
+  return (
+      <main>
+        <h1>App</h1>
+        <Input/>
+        <Modes/>
+        <Canvas/>
+      </main>
+  )
+}
